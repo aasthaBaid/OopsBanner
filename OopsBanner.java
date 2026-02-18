8a0d4378be0c41267e2342a61b5277eaf5b33c55
aasthaBaid <aasthabaid3@gmail.com>
2026-02-18 15:08:29 +0530

    adding uc3

--- OopsBanner.java
+++ OopsBanner.java
@@ -1,18 +1,18 @@
 /**
 @author developer
-@version 2
+@version 3
 */
 
-class OOPSBanner{
+class OOPSBannerStringJoin{
 	public static void main(String[] args){
-		System.out.println("    ***    " +  "     ***    " + " *******  " + "   *****  ");
-		System.out.println(" **     ** " +  "  **     ** " + " **    ** " + "  **      ");
-		System.out.println("**       **" +  " **       **" + " **     **" + " **       ");
-		System.out.println("**       **" +  " **       **" + " **    ** " + "  **      ");	
-		System.out.println("**       **" +  " **       **" + " ****     " + "   ***    ");
-		System.out.println("**       **" +  " **       **" + " **       " + "      ** ");
-		System.out.println("**       **" +  " **       **" + " **       " + "       **");
-		System.out.println(" **     ** " +  "  **     ** " + " **       " + "      ** ");
-		System.out.println("    ***    " +  "     ***    " + " **       " + "  ******  ");
+		System.out.println(String.join(" ","    ***    " , "     ***    " , " *******  " , "   *****  "));
+		System.out.println(String.join(" "," **     ** " , "  **     ** " , " **    ** " , "  **      "));
+		System.out.println(String.join(" ","**       **" , " **       **" , " **     **" , " **       "));
+		System.out.println(String.join(" ","**       **" , " **       **" , " **    ** " , "  **      "));	
+		System.out.println(String.join(" ","**       **" , " **       **" , " ****     " , "   ***    "));
+		System.out.println(String.join(" ","**       **" , " **       **" , " **       " , "      ** "));
+		System.out.println(String.join(" ","**       **" , " **       **" , " **       " , "       **"));
+		System.out.println(String.join(" "," **     ** " , "  **     ** " , " **       " , "      ** "));
+		System.out.println(String.join(" ","    ***    " , "     ***    " , " **       " , "  ******  "));
 	}
 }
