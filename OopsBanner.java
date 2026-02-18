b47b992e9a116eda24706f1adbe318cd1592b962
aasthaBaid <aasthabaid3@gmail.com>
2026-02-18 16:03:04 +0530

    adding uc5

--- OopsBanner.java
+++ OopsBanner.java
@@ -1,23 +1,21 @@
 /*
 @author developer
-@version 4
+@version 5
 */
-class OOPSBannerArray{
+class OOPSBannerAppArray{
 	public static void main(String[] args){
 		// continuous memory to store elements
-		String arr[] = new String[9];
-		//by default, all is intialised with 0
-		
-		
-		arr[0] = String.join(" ","    ***    " , "     ***    " , " *******  " , "   *****  ");
-		arr[1] = String.join(" "," **     ** " , "  **     ** " , " **    ** " , "  **      ");
-		arr[2] = String.join(" ","**       **" , " **       **" , " **     **" , " **       ");
-		arr[3] = String.join(" ","**       **" , " **       **" , " **    ** " , "  **      ");	
-		arr[4] = String.join(" ","**       **" , " **       **" , " ****     " , "   ***    ");
-		arr[5] = String.join(" ","**       **" , " **       **" , " **       " , "      ** ");
-		arr[6] = String.join(" ","**       **" , " **       **" , " **       " , "       **");
-		arr[7] = String.join(" "," **     ** " , "  **     ** " , " **       " , "      ** ");
-		arr[8] = String.join(" ","    ***    " , "     ***    " , " **       " , "  ******  ");
+		String arr[] = {
+			String.join(" ","    ***    " , "     ***    " , " *******  " , "   *****  "),
+			String.join(" "," **     ** " , "  **     ** " , " **    ** " , "  **      "),
+			String.join(" ","**       **" , " **       **" , " **     **" , " **       "),
+			String.join(" ","**       **" , " **       **" , " **    ** " , "  **      "),	
+			String.join(" ","**       **" , " **       **" , " ****     " , "   ***    "),
+			String.join(" ","**       **" , " **       **" , " **       " , "      ** "),
+			String.join(" ","**       **" , " **       **" , " **       " , "       **"),
+			String.join(" "," **     ** " , "  **     ** " , " **       " , "      ** "),
+			String.join(" ","    ***    " , "     ***    " , " **       " , "  ******  ")
+		};
 		
 		for(String str : arr){
 			System.out.println(str);
