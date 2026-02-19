9e3b3fba57ecc319530e6e4908b637bfd62dc580
aasthaBaid <aasthabaid3@gmail.com>
2026-02-19 11:51:02 +0530

    adding uc7

--- OopsBanner.java
+++ OopsBanner.java
@@ -1,10 +1,10 @@
-/*
+/**
 @author developer
-@version 6
+@version 7
 */
-class OOPSBannerFunction{
-	public static String[] getOPattern(){
-		return new String[] {
+class OOPSBannerInnerClass{
+	static class CharacterPattern{
+		static String[] oPattern = {
 			"    ***    ",
 			" **     ** ",
 			"**       **",
@@ -15,9 +15,7 @@ class OOPSBannerFunction{
 			" **     ** ",
 			"    ***    "
 		};
-	}
-	public static String[] getPPattern(){
-		return new String[] {
+		static String[] pPattern = {
 			" *******  ",
 			" **    ** ",
 			" **     **",
@@ -28,10 +26,7 @@ class OOPSBannerFunction{
 			" **       ",
 			" **       "
 		};
-		
-	}
-	public static String[] getSPattern(){
-		return new String[] {
+		static String[] sPattern = {
 			"   *****  ",
 			"  **      ",
 			" **       ",
@@ -42,16 +37,79 @@ class OOPSBannerFunction{
 			"      ** ",
 			"  ******  "
 		};
+		static String[] getPattern(char ch){
+			switch(Character.toUpperCase(ch)){
+				case 'O' : return oPattern;
+				case 'P' : return pPattern;
+				case 'S' : return sPattern;
+				default : return  new String[] {
+					" ",
+					" ",
+					" ",
+					" ",
+					" ",
+					" ",
+					" ",
+					" ",
+					" "
+				};
+			}
+		}
 	}
 	public static void main(String[] args){
-		// continuous memory to store elements
-		String oPattern[] = getOPattern();
-		String pPattern[] = getPPattern();
-		String sPattern[] = getSPattern();
+		String word = "OOPS";
+		String del = " ";
+		StringBuilder lines[] =new StringBuilder[9];
 		
-		for(int i = 0; i < oPattern.length;i++){
-			System.out.println(oPattern[i] + " " + oPattern[i] + " " + pPattern[i] + " " + sPattern[i]);
+		for(int i = 0 ; i < 9; i++){
+			lines[i] = new StringBuilder();
 		}
-		
+		for (int i = 0; i < word.length(); i++) {
+            String[] pat = CharacterPattern.getPattern(word.charAt(i));
+            for (int r = 0; r < 9; r++) {
+                lines[r].append(pat[r]);
+            }
+            if (i < word.length() - 1) {
+                for (int r = 0; r < 9; r++) lines[r].append(del);
+            }
+        }
+		for(int i = 0 ; i < 9 ;i++){
+			System.out.println(lines[i]);
+		}
 	}
 }
+
+/*
+class OOPSBannerInnerClass{
+	public static void main(String[] args){
+		Mobile.Battery battery = new Mobile().new Battery();
+		battery.showBattery();
+	}
+}
+class Mobile {
+	String brand = "Apple";
+	class Battery{
+		public void showBattery(){
+			System.out.println("Brand : " + brand);
+		}
+	}
+}
+
+class OOPSBannerInnerClass{
+	public static void main(String[] args){
+		Mobile.Battery battery = new Mobile.Battery();
+		battery.showBattery();
+	}
+}
+class Mobile {
+	static String brand = "Apple";
+	// if class is static, it cannot call non static variable, the variable has to be static
+	static class Battery{
+		public void showBattery(){
+			System.out.println("Brand : " + brand);
+		}
+	}
+}
+*/
+
+
