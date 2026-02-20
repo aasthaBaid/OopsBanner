f2c4c6e4292961edaeb1d8b386d83e6b299b3644
aasthaBaid <aasthabaid3@gmail.com>
2026-02-20 10:48:52 +0530

    adding uc8

--- OopsBanner.java
+++ OopsBanner.java
@@ -1,10 +1,13 @@
+
 /**
 @author developer
-@version 7
+@version 8
 */
-class OOPSBannerInnerClass{
-	static class CharacterPattern{
-		static String[] oPattern = {
+import java.util.*;
+class OopsBannerHash {
+	public static HashMap<Character, String[]> createHashMap(){
+		HashMap<Character, String[]> charMap = new HashMap<>();
+		charMap.put('O' , new String[] {
 			"    ***    ",
 			" **     ** ",
 			"**       **",
@@ -14,8 +17,8 @@ class OOPSBannerInnerClass{
 			"**       **",
 			" **     ** ",
 			"    ***    "
-		};
-		static String[] pPattern = {
+		});
+		charMap.put('P' , new String[] {
 			" *******  ",
 			" **    ** ",
 			" **     **",
@@ -25,8 +28,8 @@ class OOPSBannerInnerClass{
 			" **       ",
 			" **       ",
 			" **       "
-		};
-		static String[] sPattern = {
+		});
+		charMap.put('S' , new String[] {
 			"   *****  ",
 			"  **      ",
 			" **       ",
@@ -36,80 +39,21 @@ class OOPSBannerInnerClass{
 			"       **",
 			"      ** ",
 			"  ******  "
-		};
-		static String[] getPattern(char ch){
-			switch(Character.toUpperCase(ch)){
-				case 'O' : return oPattern;
-				case 'P' : return pPattern;
-				case 'S' : return sPattern;
-				default : return  new String[] {
-					" ",
-					" ",
-					" ",
-					" ",
-					" ",
-					" ",
-					" ",
-					" ",
-					" "
-				};
-			}
+		});
+		return charMap;
+		
+	}
+	
+	public static void display(String message, HashMap<Character, String[]> charMap){
+		int len = charMap.get('O').length;
+		
+		for(int i = 0 ; i < len; i++){
+			System.out.println(charMap.get('O')[i] + " " + charMap.get('O')[i] + " " + charMap.get('P')[i] + " " + charMap.get('S')[i] );
 		}
 	}
 	public static void main(String[] args){
 		String word = "OOPS";
-		String del = " ";
-		StringBuilder lines[] =new StringBuilder[9];
-		
-		for(int i = 0 ; i < 9; i++){
-			lines[i] = new StringBuilder();
-		}
-		for (int i = 0; i < word.length(); i++) {
-            String[] pat = CharacterPattern.getPattern(word.charAt(i));
-            for (int r = 0; r < 9; r++) {
-                lines[r].append(pat[r]);
-            }
-            if (i < word.length() - 1) {
-                for (int r = 0; r < 9; r++) lines[r].append(del);
-            }
-        }
-		for(int i = 0 ; i < 9 ;i++){
-			System.out.println(lines[i]);
-		}
+		HashMap<Character, String[]> charMap = createHashMap();
+		display(word , charMap);
 	}
 }
-
-/*
-class OOPSBannerInnerClass{
-	public static void main(String[] args){
-		Mobile.Battery battery = new Mobile().new Battery();
-		battery.showBattery();
-	}
-}
-class Mobile {
-	String brand = "Apple";
-	class Battery{
-		public void showBattery(){
-			System.out.println("Brand : " + brand);
-		}
-	}
-}
-
-class OOPSBannerInnerClass{
-	public static void main(String[] args){
-		Mobile.Battery battery = new Mobile.Battery();
-		battery.showBattery();
-	}
-}
-class Mobile {
-	static String brand = "Apple";
-	// if class is static, it cannot call non static variable, the variable has to be static
-	static class Battery{
-		public void showBattery(){
-			System.out.println("Brand : " + brand);
-		}
-	}
-}
-*/
-
-
